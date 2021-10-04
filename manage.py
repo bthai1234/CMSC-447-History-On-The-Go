4eb7f00b6d57eaf693bbb27554bb5cd458f21a8b
Ben Thai <ben.thai1234@gmail.com>
2021-10-04 14:08:08 -0400

    Renamed project name

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'historical_tour_site.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'history_on_the_go.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
