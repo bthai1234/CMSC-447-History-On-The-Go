9b7b3dd018460f8865f775c73fd1de241988cfef
phamthinh14 <phamthinh1994@gmail.com>
2021-10-22 23:47:28 -0400

    Able to register and add data to database

--- tour_app/urls.py
+++ tour_app/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'tour_app'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.registerPage, name='registerPage'),
+    path('login/', views.loginPage, name='loginPage'),
 ]
--- tour_app/views.py
+++ tour_app/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
+
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
 def index(request):
     return render(request, 'tour_app/index.html', context={})
+
+
+def registerPage(request):
+    regis_form = RegisterUserForm();
+    if request.method == 'POST':
+        regis_form = RegisterUserForm(request.POST)
+        if regis_form.is_valid():
+            regis_form.save()
+    return render(request, 'tour_app/registerPage.html', context={'regis_form': regis_form})
+
+
+def loginPage(request):
+    return render(request, 'tour_app/loginPage.html', context={})
