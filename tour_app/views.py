0081213516cf924ba549cb621364d3fbf7cd254c
phamthinh14 <phamthinh1994@gmail.com>
2021-10-28 00:18:37 -0400

    Able to register, login. Redirect function worked

--- tour_app/views.py
+++ tour_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import RegisterUserForm
+from django.contrib import messages
+from django.urls import reverse
 
 from django.contrib.auth import authenticate, login, logout
 
@@ -12,13 +15,31 @@ def index(request):
 
 
 def registerPage(request):
-    regis_form = RegisterUserForm();
+    regis_form = RegisterUserForm()
+    context = {'regis_form': regis_form}
     if request.method == 'POST':
         regis_form = RegisterUserForm(request.POST)
         if regis_form.is_valid():
             regis_form.save()
-    return render(request, 'tour_app/registerPage.html', context={'regis_form': regis_form})
+            return HttpResponseRedirect(reverse('tour_app:loginPage'))
+    return render(request, 'tour_app/registerPage.html', context)
 
 
 def loginPage(request):
-    return render(request, 'tour_app/loginPage.html', context={})
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('tour_app:index'))
+
+    context = {}
+    return render(request, 'tour_app/loginPage.html', context)
+
+
+def regis_success(request):
+    context = {}
+    return render(request, 'tour_app/regis_success.html', context)
