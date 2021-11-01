63d45ad7e871b7015cbee76b13b2c1f175012385
Ben Thai <ben.thai1234@gmail.com>
2021-10-31 22:10:14 -0400

    Made seprate directory of java script unit test views

--- history_on_the_go/settings.py
+++ history_on_the_go/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'history_on_the_go.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'tour_app/js_tests/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
